b93f2c443fbca64e13164b6002d6dfff34b26fe8
RmnDx <ramandeepkaur.dx@gmail.com>
2024-03-18 18:48:56 +0530

    section

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -52,7 +52,6 @@ const CompanyRead = lazy(() => import('@/pages/Company/CompanyRead'));
 const CompanyUpdate = lazy(() => import('@/pages/Company/CompanyUpdate'));
 const About = lazy(() => import('@/pages/About'));
 const Verify = lazy(() => import('@/pages/Verify'));
-const SamplePeople = lazy(() => import('@/pages/SamplePeople'));
 
 const PricingModel = lazy(() => import('@/pages/PricingModel'));
 const SubscriptionType = lazy(() => import('@/pages/SubscriptionType'));
@@ -271,10 +270,6 @@ export default function AppRouter() {
     {
       path: '*',
       element: <NotFound />,
-    },
-    {
-      path: '/sample',
-      element: <SamplePeople />,
     },
   ]);
 
