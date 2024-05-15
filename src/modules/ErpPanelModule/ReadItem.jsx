af8eb851c6e90bef3ad99cffab13fb258d031f9c
Manisharanidx <manisharani.dx@gmail.com>
2024-05-15 17:48:48 +0530

    15MAY workorderUI

--- src/modules/ErpPanelModule/ReadItem.jsx
+++ src/modules/ErpPanelModule/ReadItem.jsx
@@ -388,7 +388,7 @@ export default function ReadItem({ config, selectedItem }) {
               <Col span={12}>
                 <Row className="gutter-row" >
                   <Col span={12}>
-                    <p style={{ fontSize: "15px", fontWeight: "600" }}>{translate('Sales Person:')} .:<br></br>
+                    <p style={{ fontSize: "15px", fontWeight: "600" }}>{translate('Sales Person')} :<br></br>
                     </p></Col>
                   <Col span={12}>
                     <p style={{ fontSize: "13px", fontWeight: "600", color: "#a3a3a3", marginTop: "19px" }}>{translate('Super Admin')} <br></br>
@@ -434,22 +434,23 @@ export default function ReadItem({ config, selectedItem }) {
         </Col>
 
       </Row>
+      {/* ................. */}
       <h3>WorkOrder Billing Detail</h3>
       <Row className="gutter-row" style={{ marginTop: "20px" }}>
         <Col span={24}>
-          <div className='' style={{ width: "100%", border: "1px solid rgb(240,240,240)", height: "257px", borderRadius: "5px", boxShadow: "0px 0px 0px 1px rgb(240,240,240)" }}>
+          <div className='' style={{ width: "100%", border: "1px solid rgb(240,240,240)", height: "360px", borderRadius: "5px", boxShadow: "0px 0px 0px 1px rgb(240,240,240)" }}>
 
             <Row gutter={[12 - 12]} style={{ padding: "0px 0px 0px 20px" }}>
               <Col className="gutter-row" span={12}>
-                <p style={{ fontSize: "15px", fontWeight: "600" }}>{translate('Bill To')} :<br></br>
-                  <span style={{ fontSize: "13px", color: "#a3a3a3" }}> Jane</span></p>
+                <p style={{ fontSize: "15px", fontWeight: "600" }}>{translate('Workorder For')} :<br></br>
+          </p>
 
               </Col>
 
               <Col className="gutter-row" span={12}>
-                <p style={{ fontSize: "15px" }}>
-                  {translate('Contact Person')} :<br></br>
-                  <span style={{ fontSize: "13px", color: "#a3a3a3" }}> 132545</span>
+                <p style={{ fontSize: "14px", color: "#a3a3a3" }}>
+                  {translate('Lawn Care:One Time')} <br></br>
+                  
                 </p>
 
 
@@ -458,37 +459,75 @@ export default function ReadItem({ config, selectedItem }) {
 
               <Col className="gutter-row" span={12}>
                 <p style={{ fontSize: "15px" }} >
-                  {translate('Bill To Contact')} :<br></br>
-                  <span style={{ fontSize: "13px", color: "#a3a3a3" }}> Tel: 46747474</span> <br></br>
-                  <span style={{ fontSize: "13px", color: "#a3a3a3" }}> Email: Jane@email.com</span>
-                </p>
-
-              </Col>
+                  {translate('Per Workorder Cost')} :<br></br>
+                  
+                  </p> </Col>
+              <Col className="gutter-row" span={12}>
+                <p style={{ fontSize: "14px", color: "#a3a3a3" }} >
+                  {translate('300.00 /Workorder')} <br></br>
+                  </p> </Col>
 
               <Col className="gutter-row" span={12}>
                 <p style={{ fontSize: "15px" }}>
-                  {translate('Work Site Contact')} :<br></br>
-                  <span style={{ fontSize: "13px", color: "#a3a3a3" }}> Tel: 46747474</span>
-                </p>
-
-
-              </Col>
+                  {translate('Adjustment')} :<br></br>
+                
+                </p> </Col>
+              <Col className="gutter-row" span={12}>
+                <p style={{ fontSize: "14px", color: "#a3a3a3" }}>
+                  {translate(`${(+10.00)}`)} <br></br>
+                
+                </p> </Col>
 
 
               <Col className="gutter-row" span={12} >
-                <p style={{ fontSize: "15px" }}>{translate('Billing Address')} :
+                <p style={{ fontSize: "15px" }}>{translate(`Discount1`)} :
                   <br></br>
-                  <span style={{ fontSize: "13px", color: "#a3a3a3" }}> block,street,unit,Armenia,2662</span>
+                 
+                </p>
+
+              </Col>
+              <Col className="gutter-row" span={12} >
+                <p style={{ fontSize: "14px", color: "#a3a3a3" }}>{translate('3.06')} 
+                  <br></br>
+                 
                 </p>
 
               </Col>
               <Col className="gutter-row" span={12}>
                 <p style={{ fontSize: "15px" }}>
-                  {translate('Work Address')} :<br></br>
-                  <span style={{ fontSize: "13px", color: "#a3a3a3" }}> block,street,unit,Armenia,2662</span>
+                  {translate('Package SubTotal')} :<br></br>
+              
                 </p>
-
-
+           </Col>
+              <Col className="gutter-row" span={12}>
+                <p style={{ fontSize: "14px", color: "#a3a3a3" }}>
+                  {translate('306.90/One Time')} <br></br>
+                 
+                </p>
+              </Col>
+              <Col className="gutter-row" span={12}>
+                <sp style={{ fontSize: "15px" }}>
+                  {translate('Tax 20')} :<br></br>
+              
+                </sp>
+              </Col>
+              <Col className="gutter-row" span={12}>
+                <p style={{ fontSize: "14px", color: "#a3a3a3" }}>
+                  {translate('61.38')} <br></br>
+                
+                </p>
+              </Col>
+              <Col className="gutter-row" span={12}>
+                <p style={{ fontSize: "15px" }}>
+                  {translate('Total Package Cost')} :<br></br>
+                
+                </p>
+              </Col>
+              <Col className="gutter-row" span={12}>
+                <p style={{ fontSize: "14px", color: "#a3a3a3" }}>
+                  {translate('368.28/One Time')} <br></br>
+                 
+                </p>
               </Col>
 
             </Row>
@@ -496,7 +535,7 @@ export default function ReadItem({ config, selectedItem }) {
         </Col>
       </Row>
       {/* .................. */}
-      <Row className="gutter-row" style={{ marginTop: "20px" }}>
+      {/* <Row className="gutter-row" style={{ marginTop: "20px" }}>
         <Col span={12}>
           <h3>Other detail</h3>
           <div className='' style={{ width: "97%", border: "1px solid rgb(240,240,240)", height: "240px", borderRadius: "5px", boxShadow: "0px 0px 0px 1px rgb(240,240,240)" }}>
@@ -509,6 +548,154 @@ export default function ReadItem({ config, selectedItem }) {
           <div className='' style={{ width: "100%", border: "1px solid rgb(240,240,240)", height: "240px", borderRadius: "5px", boxShadow: "0px 0px 0px 1px rgb(240,240,240)" }}>
 
           </div>
+
+        </Col>
+
+      </Row> */}
+      <h3>Basic WorkOrder Details</h3>
+      <Row className="gutter-row">
+        <Col span={24}>
+          <div className='' style={{ width: "100%", border: "1px solid rgb(240,240,240)", height: "400px", borderRadius: "5px", boxShadow: "0px 0px 0px 1px rgb(240,240,240)" }}>
+            <Row className="gutter-row" style={{ padding: "0px 0px 0px 20px", marginTop: "20px" }}>
+              <Col span={[8]}>
+                <Col className="gutter-row" span={12}>
+                  <p style={{ fontSize: "15px", fontWeight: "600" }}>{translate('Attendance Type')}:<br></br>
+                  </p></Col>
+              </Col>
+              <Col span={[12]}>
+                <Col className="gutter-row" span={12}>
+                  <p style={{ fontSize: "15px", fontWeight: "600",  marginLeft:"-33%"}}>{translate('Independent Attendance')} <br></br>
+                  </p></Col>
+              
+               
+              </Col>
+            </Row>
+            <div className='' style={{ width: "98%", border: "1px solid rgb(240,240,240)", height: "300px", borderRadius: "0px", boxShadow: "0px 0px 0px 1px #0e0e0e", marginLeft:"1%"}}>
+            <Row className="gutter-row" style={{ padding: "0px 0px 0px 0px", marginTop: "10px" }}>
+             
+                  <Col span={24} >
+                  <Row style={{ padding: "0px 0px 0px 20px" }}>
+                    <Col span={6}>
+                      <p style={{ fontSize: "15px", fontWeight: "600",  }}>{translate('Worker')} <br></br>
+                    </p>
+                    </Col>
+                    <Col span={6}> <p style={{ fontSize: "15px", fontWeight: "600", }}>{translate('Expected Time')} <br></br>
+                    </p></Col>
+                    <Col span={6}> <p style={{ fontSize: "15px", fontWeight: "600", }}>{translate('Actual Time')} <br></br>
+                    </p></Col>
+                    
+
+                    <Col span={6}>
+                      <Row style={{ padding: "0px 0px 0px 20px" }}>
+                        <Col span={4}>
+                          <p style={{ fontSize: "15px", fontWeight: "600", }}>{translate('Status')} <br></br>
+                          </p>
+                      </Col>
+                        {/* <Col span={2}>
+                          4
+                        </Col> */}
+                    </Row >
+                    </Col>
+                    </Row>
+                    <hr ></hr>
+                  <Row style={{ padding: "0px 0px 0px 20px" }}>
+                    <Col span={6}>
+                      <p style={{ fontSize: "15px", fontWeight: "600", }}>{translate('Test Leader')} <br></br>
+                        <span style={{ fontSize: "12px", color:"#7928dd"}}>Leader</span>
+                      </p>
+                    
+                    </Col>
+                    <Col span={6}> <p style={{ fontSize: "15px", fontWeight: "600", }}>09:00 AM-01:00 PM <br></br>
+                    </p></Col>
+                    <Col span={6}> <p style={{ fontSize: "15px", fontWeight: "600", }}> <br></br>
+                    </p></Col>
+
+
+                    <Col span={6}>
+                      <Row>
+                        <Col span={4}>
+                          <p style={{
+                            fontSize: "14px", fontWeight: "600", color:
+                              "#fdf2ff", backgroundColor: "#faad09", width: "90px",
+                            textAlign: "center", padding: "4px 4px 4px 4px", borderRadius: "14px"
+                          }}>
+                            {translate('Scheduled')} <br></br>
+                          </p>
+                        </Col>
+                        {/* <Col span={2}>
+                          4
+                        </Col> */}
+                      </Row>
+                    </Col>
+                  </Row>
+                  <hr></hr>
+                  <Row style={{ padding: "0px 0px 0px 20px" }} >
+                    <Col span={6}>
+                      <p style={{ fontSize: "15px", fontWeight: "600", }}>{translate('Alfreda Byers')} <br></br>
+                      </p>
+                    </Col>
+                    <Col span={6}> <p style={{ fontSize: "15px", fontWeight: "600", }}>09:00 AM-01:00 PM <br></br>
+                    </p></Col>
+                    <Col span={6}> <p style={{ fontSize: "15px", fontWeight: "600", }}><br></br>
+                    </p></Col>
+
+
+                    <Col span={6}>
+                      <Row>
+                        <Col span={4}>
+                          <p style={{
+                            fontSize: "14px", fontWeight: "600", color:
+                              "#fdf2ff", backgroundColor: "#faad09", width: "90px",
+                            textAlign: "center", padding: "4px 4px 4px 4px", borderRadius: "14px"
+                          }}>
+                            {translate('Scheduled')} <br></br>
+                          </p>
+                        </Col>
+                        {/* <Col span={2}>
+                          4
+                        </Col> */}
+                      </Row>
+                    </Col>
+                  </Row>
+                  <hr></hr>
+                  <Row style={{ padding: "0px 0px 0px 20px" }}>
+                    <Col span={6}>
+                      <p style={{ fontSize: "15px", fontWeight: "600", }}>{translate('Carly Alvarez')} <br></br>
+                      </p>
+                    </Col>
+                    <Col span={6}> <p style={{ fontSize: "15px", fontWeight: "600", }}>09:00 AM-01:00 PM<br></br>
+                    </p></Col>
+                    <Col span={6}> <p style={{ fontSize: "15px", fontWeight: "600", }}>05:30 AM<br></br>
+                    </p></Col>
+
+
+                    <Col span={6}>
+                      <Row>
+                        <Col span={4}>
+                          <p style={{
+                            fontSize: "14px", fontWeight: "600", color:
+                              "#fdf2ff", backgroundColor: "#1963b5", width: "90px",
+                            textAlign: "center", padding: "4px 4px 4px 4px", borderRadius: "14px"
+                          }}>
+                            {translate('In-Progress')} <br></br>
+                          </p>
+                        </Col>
+                        {/* <Col span={2}>
+                          4
+                        </Col> */}
+                      </Row>
+                    </Col>
+                  </Row>
+                  </Col>
+
+              </Row>
+              </div>
+                
+
+              
+            
+          </div>
+
 
         </Col>
 
